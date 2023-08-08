028654cd56182127d5f5281001ca040536364fc6
mrk-hnr <mrk.hnr@hotmail.com>
2023-08-08 21:04:19 +0800

    Added nodemon

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ app.listen(3000, function() {
     console.log("Listening on port 3000") // Allows us to view it on browser via localhost: XXXX
 })
 
-// GET
+// +++++++++++++++++++++
+// ++++ GET SECTION ++++
+// +++++++++++++++++++++
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html") //sends the specific directory plus the file inside the quotation mark
+    res.sendFile(__dirname + "/index.html") //sends the specific directory plus the file inside the quotation mark, wherein __dirname is the current directory we are in.
 })
